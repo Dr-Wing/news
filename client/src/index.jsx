aacae79bdb797c35f6308f21e29bf03a6c18667c
Matthew Goyke <mwgoyke@gmail.com>
2019-11-21 16:44:06 -0800

    webpack, babel, html, and react setup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,17 @@
 console.log('loaded')
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+        <div>app rendered</div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,23 @@ const path = require('path');
 
 module.exports = {
   entry: './client/src/index.jsx',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './client/dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
 };
